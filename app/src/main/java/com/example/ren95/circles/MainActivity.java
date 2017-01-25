0e234b733826418186e4ed279d59dd2fde8deaa6
Shakurov Renat <traphan7182@gmail.com>
2017-01-25 14:18:21 +0300

    curswork3
    q

--- app/src/main/java/com/example/ren95/circles/MainActivity.java
+++ app/src/main/java/com/example/ren95/circles/MainActivity.java
@@ -11,9 +11,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+      /*  getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-     getSupportActionBar().hide();
+*/     getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 }
